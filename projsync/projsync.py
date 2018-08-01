515d0674d7a44e31c10575d0fc4258ad9573ae5d
Jean-Nicolas Guiavarc'h <jean-nicolas.guiavarch@myscript.com>
2018-08-01 10:42:46 +0200

    Update mod-pbxproj to version 2.5.1
    Move additions to pbxproj to projsync
    Align XcodeProj and VCXProj list-files output so that they can be compared using standard diff tools

--- projsync/projsync.py
+++ projsync/projsync.py
@@ -25,25 +25,47 @@ class Project:
         else:
             raise Exception("Unsupported project type (" + ext + ")")
 
+    def _list_repr(self, array, sep=';'):
+        str = ""
+        for s in array:
+            if len(str) > 0:
+                str = str + sep
+            if s:
+                str = str + s
+            else:
+                str = str + '<none>'
+        return str
+
 
 class XcodeProj(Project):
 
     def __init__(self, impl):
         self.impl = impl
+        self.parents = {}
+        groups = impl.objects.get_objects_in_section(u'PBXGroup')
+        for group in groups:
+            for id in group.children:
+                self.parents[id] = group
 
     @classmethod
     def Load(cls, path):
-        impl = mod_pbxproj.XcodeProject.Load(path, pure_python=True)
+        impl = mod_pbxproj.XcodeProject.load(path)
         return XcodeProj(impl)
+
+    def get_parent(self, obj):
+        id = obj.get_id()
+        if id in self.parents:
+            return self.parents[id]
+        return None
 
     def children(self, project, group):
         for child_id in group['children']:
-            yield project.get_obj(child_id)
+            yield project.get_object(child_id)
 
     def child_groups(self, project, group):
         for child_id in group['children']:
-            child = project.get_obj(child_id)
-            if child.get('isa') == 'PBXGroup':
+            child = project.get_object(child_id)
+            if child.isa == 'PBXGroup':
                 yield child
 
     def print_group(self, project, group, prefix=""):
@@ -53,10 +75,75 @@ class XcodeProj(Project):
 
     def find_referenced_project(self, targetName):
         for k, v in self.impl.objects.iteritems():
-            if v.get('isa') == 'PBXContainerItemProxy' and v.get('remoteInfo') == targetName:
-                fileRef = self.impl.get_obj(v.get('containerPortal'))
+            if v.isa == 'PBXContainerItemProxy' and v.remoteInfo == targetName:
+                fileRef = self.impl.get_object(v.containerPortal)
                 path = self.impl._resolve_path(fileRef)
-                return mod_pbxproj.XcodeProject.Load(os.path.join(path, "project.pbxproj"), pure_python=True)
+                return mod_pbxproj.XcodeProject.Load(os.path.join(path, "project.pbxproj"))
+        return None
+
+    def get_target_source_files(self, target):
+        for id in target.buildPhases:
+            buildPhase = self.impl.get_object(id)
+            if buildPhase.isa == 'PBXSourcesBuildPhase':
+                for f in buildPhase.files:
+                    buildFile = self.impl.get_object(f)
+                    fileRef = self.impl.get_object(buildFile.fileRef)
+                    yield fileRef
+
+    def get_group_files(self, group):
+        for id in group.children:
+            item = self.impl.get_object(id)
+            if item.isa == 'PBXGroup':
+                for f in self.get_group_files(item):
+                    yield f
+            else:
+                yield item
+
+    def _get_object_path(self, obj):
+        if obj and u'path' in obj:
+            return obj.path
+        return None
+
+    def resolve_path(self, fileRef, directory=None):
+        path = self._resolve_path(fileRef)
+        path = os.path.realpath(path)
+        return os.path.relpath(path, directory)
+
+    def _resolve_path(self, fileRef):
+        sourceTree = fileRef.sourceTree
+        path = self._get_object_path(fileRef)
+        if sourceTree == '<absolute>':
+            return path
+        elif sourceTree == '<group>':
+            group = self.get_parent(fileRef)
+            groupPath = self._get_object_path(group)
+            while not groupPath and group:
+                group = self.get_parent(group)
+                groupPath = self._get_object_path(group)
+            if groupPath is not None:
+                groupPath = os.path.join(self.impl._source_root, groupPath)
+                return os.path.join(groupPath, path)
+            else:
+                return os.path.join(self.impl._source_root, path)
+        elif sourceTree == 'SOURCE_ROOT':
+            return os.path.join(self.impl._source_root, path)
+        else:
+            raise Exception("Unsupport sourceTree " + sourceTree)
+
+    def get_file_groups(self, fileRef, rootGroup):
+        groups = []
+        group = self.get_parent(fileRef)
+        while group and group != rootGroup:
+            groups.insert(0, group.get_name())
+            group = self.get_parent(group)
+        return groups
+
+    def get_target_group(self, targetName):
+        groups = self.impl.get_groups_by_name(targetName)
+        for group in groups:
+            parent = self.get_parent(group)
+            if not (parent and u'name' in parent):
+                return group
         return None
 
     def list_files(self, targetName, directory=os.curdir):
@@ -64,52 +151,41 @@ class XcodeProj(Project):
             project = self.impl
 
             # get the target
-            targets = project.get_targets_by_name(targetName)
-            if len(targets) < 1:
-                project = self.find_referenced_project(targetName)
-                if project != None:
-                    targets = project.get_targets_by_name(targetName)
-            if len(targets) < 1:
-                raise Exception("No such target " + targetName)
-            elif len(targets) > 1: # is this possible?
-                raise Exception("Ambiguous target " + targetName)
-            target = targets[0]
+            target = project.get_target_by_name(targetName)
 
-            # get the corresponding group, if there is one and only one group
-            # with the name of the target
-            groups = project.get_groups_by_name(targetName)
-            if len(groups) == 1:
-                group = groups[0]
-            else:
-                group = None
+            # get the corresponding group, if there is a group with the name of the target at the root of the project
+            target_group = self.get_target_group(targetName)
+
+            groups = {}
 
             # get build files from target
-            buildSourceFiles = [f for f in project.get_target_source_files(target)]
+            buildSourceFiles = [f for f in self.get_target_source_files(target)]
             buildSourceFilePaths = []
             for f in buildSourceFiles:
-                buildSourceFilePaths.append(project.resolve_path(f, directory))
+                p = self.resolve_path(f, directory)
+                buildSourceFilePaths.append(p)
+                groups[p] = self.get_file_groups(f, target_group)
             buildSourceFilePaths.sort()
 
             # get other referenced files from group
-            if group:
-                otherReferenceFiles = [f for f in project.get_group_files(groups[0]) if not f in buildSourceFiles]
+            if target_group:
+                otherReferenceFiles = [f for f in self.get_group_files(target_group) if not f in buildSourceFiles]
             else:
                 otherReferenceFiles = []
             otherReferenceFilePaths = []
             for f in otherReferenceFiles:
-                otherReferenceFilePaths.append(project.resolve_path(f, directory))
+                p = self.resolve_path(f, directory)
+                otherReferenceFilePaths.append(p)
+                groups[p] = self.get_file_groups(f, target_group)
             otherReferenceFilePaths.sort()
 
             # list the files
-            print "===== GROUPS"
-            for group in self.child_groups(project, project.root_group):
-                self.print_group(project, group)
             print "===== BUILD SOURCE FILES:"
             for p in buildSourceFilePaths:
-                print p
+                print '{0}\t{1}'.format(p, self._list_repr(groups[p]))
             print "===== OTHER REFERENCED FILES:"
             for p in otherReferenceFilePaths:
-                print p
+                print '{0}\t{1}'.format(p, self._list_repr(groups[p]))
 
 class VCXProj(Project):
 
@@ -148,7 +224,6 @@ class VCXProj(Project):
 
         p = filters.getroot()
 
-
         groups = {}
 
         # get build source files
@@ -181,7 +256,7 @@ class VCXProj(Project):
         # list the files
         print "===== BUILD SOURCE FILES:"
         for p in buildSourceFilePaths:
-            print '{0}\t{1}'.format(p, groups[p])
+            print '{0}\t{1}'.format(p, self._list_repr(groups[p]))
         print "===== OTHER REFERENCED FILES:"
         for p in otherReferenceFilePaths:
-            print '{0}\t{1}'.format(p, groups[p])
+            print '{0}\t{1}'.format(p, self._list_repr(groups[p]))
